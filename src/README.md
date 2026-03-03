ae61cb8705aa1dc502b94ec28eaa31be59e458e6
Maria Klara Fagundes <dayasun03@gmail.com>
2026-03-03 19:46:32 -0300

    inicio do projeto

---
+++ src/README.md
@@ -0,0 +1,19 @@
+# Tópicos importantes
+
+revisão da aula passada
+- Qual a linguagem de programação utilizada para o nosso Backend?
+- O que é Node.js?
+- "     " NPM?
+- "     " Express.js?
+
+24.02
+- Como mudar o formato de anotação de require(padrão commonjs) para module?
+se muda no type do packend.json
+
+- O quqe é um API?
+- O que é HTTP?
+- Quais os métodos de requisição HTTP e para que serve cada umas delas?
+- GET, POST, PUT, PATCH, DELETE
+- Create, Read, Update, Delete (CRUD)
+- Extenções usadas : bruno, draw.io
+
--- src/server.js
+++ src/server.js
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/', (req, res) => {
+  res.send('Exemplo de Get na rota /user')
+})
+
+app.post('/', (req, res) => {
+  res.send('Exemplo de Get na rota /user')
+})
+
+app.put('/', (req, res) => {
+  res.send('Exemplo de Get na rota /user')
+})
+
+app.patch('/', (req, res) => {
+  res.send('Exemplo de Get na rota /user')
+})
+
+app.delete('/', (req, res) => {
+  res.send('Exemplo de Get na rota /user')
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Servidor rodando no http://localhost${port}`)
 })
